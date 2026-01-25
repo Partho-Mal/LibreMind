fd5e10f5e1716e0bc6ef2491bd9eb9f9b2f49c16
Ritesh Maurya <maurya.ritesh2005@gmail.com>
2026-01-25 16:52:03 +0530

    Update homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,16 +2,16 @@ import HeroSection from "@/sections/HeroSection";
 import FeaturesSection from "@/sections/FeaturesSection";
 import TestimonialSection from "@/sections/TestimonialSection";
 import CTASection from "@/sections/CTASection";
-// import ContactSection from "@/sections/ContactSection";
+import ContactSection from "@/sections/ContactSection";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col">
       <HeroSection />
       <FeaturesSection />
-      <TestimonialSection />
+      <TestimonialSection />  
       <CTASection />
-      {/* <ContactSection /> */}
+      <ContactSection />
     </div>
   );
 }
